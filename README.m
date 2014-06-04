138c80ff2b27a0dd7533f500dcba3f1a4c71b9fa
Eeshit Dhaval Vaishnav <eeshit.vaishnav@gmail.com>
2014-06-03 22:08:18 -0400

    progress,
    My guess is that the images have not been edged properly

---
+++ README.m
@@ -0,0 +1,20 @@
+if 0
+Image1_011113
+xy = 0.13
+t = 8.4
+edged membrane = z005
+injected frame 5
+
+Image5_011113
+xy = 0.13
+t = 8.3
+edged membrane = z008
+injected frame 30
+
+Image7_121311
+xy = 0.12
+t = 24.3
+edged membrane = z007
+injected frame = 1
+
+end
---
+++ master.m
@@ -0,0 +1,1 @@
+%%image. You need to enter the path of the image in the first line of code.
